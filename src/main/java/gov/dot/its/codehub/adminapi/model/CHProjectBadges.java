ba0a0caf29f18fd70966b3fc5b113b5b2316dd00
Gio <rodriguez_carlos@bah.com>
2019-12-11 10:56:52 -0500

    RDA-1349 : Add support for featured property
    - featured property gets its value from badges.featured.

--- src/main/java/gov/dot/its/codehub/adminapi/model/CHProjectBadges.java
+++ src/main/java/gov/dot/its/codehub/adminapi/model/CHProjectBadges.java
@@ -1,7 +1,16 @@
 package gov.dot.its.codehub.adminapi.model;
 
 public class CHProjectBadges {
+	private boolean featured;
 	private String status;
+
+	public boolean isFeatured() {
+		return featured;
+	}
+
+	public void setFeatured(boolean featured) {
+		this.featured = featured;
+	}
 
 	public String getStatus() {
 		return status;
