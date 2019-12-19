b23a775a6243231bc104c02901d4ea6ae159d2a1
Gio <rodriguez_carlos@bah.com>
2019-12-19 18:45:38 -0500

    RDA-1391 : Changes to adjust to new index schema in ElasticSearch.

---
+++ src/main/java/gov/dot/its/codehub/adminapi/model/CHGeneratedData.java
@@ -0,0 +1,13 @@
+package gov.dot.its.codehub.adminapi.model;
+
+public class CHGeneratedData {
+	private long rank;
+
+	public long getRank() {
+		return rank;
+	}
+	public void setRank(long rank) {
+		this.rank = rank;
+	}
+
+}
